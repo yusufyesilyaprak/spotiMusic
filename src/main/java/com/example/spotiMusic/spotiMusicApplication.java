92fce01275ecf02bb659b5fd668857b503cae1e7
yusufyesilyaprak <yusufyesilyaprakk@gmail.com>
2026-08-05 00:41:44 +0300

    H2 sunucusu icin properties duzenlemeleri

--- src/main/java/com/example/spotiMusic/spotiMusicApplication.java
+++ src/main/java/com/example/spotiMusic/spotiMusicApplication.java
@@ -1,13 +1,13 @@
-package com.example.spotiMusic.demo;
+package com.example.spotiMusic;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DemoApplication {
+public class spotiMusicApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(DemoApplication.class, args);
+		SpringApplication.run(spotiMusicApplication.class, args);
 	}
 
 }
--- src/test/java/com/example/spotiMusic/spotiMusicApplicationTests.java
+++ src/test/java/com/example/spotiMusic/spotiMusicApplicationTests.java
@@ -1,10 +1,10 @@
-package com.example.spotiMusic.demo;
+package com.example.spotiMusic;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class DemoApplicationTests {
+class spotiMusicApplicationTests {
 
 	@Test
 	void contextLoads() {
